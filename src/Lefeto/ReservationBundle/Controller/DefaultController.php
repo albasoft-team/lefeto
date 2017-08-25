1991df0cfd79696aa2a220e97d276ad8e71b021c
albasoft-team <ibrahima-sory.diallo@albasoft.fr>
2017-08-25 11:23:18 +0000

    first commit

---
+++ src/Lefeto/ReservationBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lefeto\ReservationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('ReservationBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Lefeto/ReservationBundle/ReservationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lefeto\ReservationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ReservationBundle extends Bundle
+{
+}
