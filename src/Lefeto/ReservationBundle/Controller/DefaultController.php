b5dcd9d9b4184e74141c9ef296ded8f2382eb76b
albasoft-team <ibrahima-sory.diallo@albasoft.fr>
2017-09-05 10:44:41 +0000

    integration du template

--- src/Lefeto/ReservationBundle/Controller/DefaultController.php
+++ src/Lefeto/ReservationBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ReservationBundle:Default:index.html.twig');
+        return $this->render('base.html.twig');
     }
 }
